e961add3ea9f18961e9ebe8b9ba4330765a08371
milandrovna <milandrovna@gmail.com>
2025-09-30 13:35:09 +0300

    Apply Material 3 color scheme (theme only)

--- app/src/main/java/ee/ut/cs/budgetly/ui/theme/Color.kt
+++ app/src/main/java/ee/ut/cs/budgetly/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Plum            = Color(0xFF4B2E57) // primary
+val PlumContainer   = Color(0xFF6A4E78) // primary container
+val Mauve           = Color(0xFF8B75A1) // secondary
+val Lavender        = Color(0xFFB69CD9) // tertiary
+
+val CreamBg         = Color(0xFFF3EEE6) // background
+val CardSurface     = Color(0xFFF7F1EA) // surface / cards
+val TextMain        = Color(0xFF2B2333) // onSurface
+
+val ErrorRed        = Color(0xFFD64545) // error
+val SuccessGreen    = Color(0xFF4CAF50) // custom success

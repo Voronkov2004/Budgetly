3e2d973a4a41360dc3b00391d63c11d57d604365
milandrovna <milandrovna@gmail.com>
2025-11-09 16:45:23 +0200

    Added icon and splash screen

--- app/src/main/java/ee/ut/cs/budgetly/MainActivity.kt
+++ app/src/main/java/ee/ut/cs/budgetly/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ee.ut.cs.budgetly.nav.BudgetlyNavGraph
 import ee.ut.cs.budgetly.ui.theme.BudgetlyTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
